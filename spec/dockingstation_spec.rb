75c1035bc24f3ac731e6743483ca182377568b57
Peter O <harmless_goose@icloud.com>
2022-01-25 16:54:18 +0000

    Added first RSpec test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../lib/dockingstation.rb'
 
 describe DockingStation do
-  
+  describe "#release_bike" do
+    #   it 'releases a bike' do
+    #     dockingstation = DockingStation.new
+    #     expect(dockingstation).to respond_to(release_bike)
+    #   end
+
+    it { is_expected.to respond_to :release_bike }
+  end
 end
